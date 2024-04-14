815b2716d867fb5897dd907375c5de8b020f22c2
Djordje <djordje.thai@gmail.com>
2024-04-14 09:13:08 +0700

    ver 2

--- pitanja.py
+++ pitanja.py
@@ -2,6 +2,7 @@ import streamlit as st
 import json
 import re
 
+# proverava ispravnost email adrese
 def is_valid_email(email):
     # Regular expression pattern for validating an email
     email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
@@ -18,11 +19,11 @@ def load_questions(opcija):
         questions = json.load(file)
     return questions
 
-# Streamlit app layout
+# Streamlit app layout - univerzalni formular
 def odgovori(opcija):
     st.subheader(opcija)
 
-    # Load the questions
+    # Load the questions na osnovu odabrane vrste upitnika
     questions = load_questions(opcija)
 
     # Dictionary to store answers
@@ -36,18 +37,20 @@ def odgovori(opcija):
 
         # For choice questions (single or multiple options)
         if answer_type == 'choice':
-            if any("Molimo navedite" in option for option in options):
-                # Assuming "Other [Please specify]" needs a text input
-                responses[question_text] = st.multiselect(question_text, options[:-1])
-                responses[question_text].append(st.text_input("Please specify if 'Other'"))
-            else:
                 responses[question_text] = st.selectbox(question_text, options)
-        # For free text questions
+        # za multiselect i mogucnost upisa dodatnog odgovora
+        elif answer_type == 'multichoice':
+                responses[question_text] = st.multiselect(question_text, options[:-1], placeholder = "Mozete odabrati vise opcija i upisati nove ")
+                responses[question_text].append(st.text_input(f"Navedite dodatni odgovor za pitanje {question_text}"))
+        # za tekstualne odgovore        
         elif answer_type == 'opis':
             responses[question_text] = st.text_area(question_text)
+
     email = st.text_input("Unestite email (obavezno polje):")
     # Submit button
     if st.button('Submit') and is_valid_email(email):
+        with st.expander("Odgovori"):
+            st.write(responses)
         # Here you can further process responses or save them
         return responses, email
     else:
--- upitnik.py
+++ upitnik.py
@@ -10,6 +10,23 @@ import numpy as np
 client=OpenAI()
 avatar_ai="bot.png" 
 
+# agent llm odgovara na razlicite upite
+def positive_agent(messages):
+    with st.chat_message("assistant", avatar=avatar_ai):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model="gpt-4-turbo-preview",
+            messages=messages,
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+        
+    return full_response
+
+# privremeni grafikon
 def create_radar_chart(data, labels, num_vars):
     angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
     data += data[:1]
@@ -21,9 +38,22 @@ def create_radar_chart(data, labels, num_vars):
     ax.set_yticklabels([])
     ax.set_xticks(angles[:-1])
     ax.set_xticklabels(labels)
-
     return fig
 
+# cuva grafikon
+def show_graph():
+    st.title('Polar Chart Example')
+    labels = ['Poslovna zrelostt', 'Digitalna zrelost', 'Upotreba AI', 'Sajber bezbednost', 'IT infrastruktura']
+    data = [4, 3, 4, 2, 5]
+    num_vars = len(data)
+    # Plotting
+    radar_fig = create_radar_chart(data, labels, num_vars)
+    radar_fig.savefig('radar_chart.png', bbox_inches='tight')
+    # Display in Streamlit
+    st.pyplot(radar_fig)
+    return 'radar_chart.png'
+
+# salje email
 def posalji_mail(email,gap_analiza, image_path):
     st.info(f"Saljem email na adresu {email}")
     send_email(
@@ -38,15 +68,17 @@ def posalji_mail(email,gap_analiza, image_path):
             image_path= image_path
             )
     st.info(f"Poslat je email na adresu {email}")
-    
+ 
+# RAG pretrazuje index za preporuke    
 def recommended(full_response):
     processor = HybridQueryProcessor(namespace="positive", top_k=3)
     return processor.process_query_results(full_response)
 
+# glavni program
 def main():
     with st.sidebar:
-        st.caption("Ver. 13.04.24" )
-        st.subheader("Demo GAP sa slanjem maila ")
+        st.caption("Ver. 14.04.24" )
+        st.subheader("Demo GAP sa grafikonon i slanjem maila ")
         opcija = st.selectbox("Odaberite upitnik", ("",
                                                     "Opsti", 
                                                     "Poslovna zrelost", 
@@ -57,50 +89,24 @@ def main():
     if opcija !="":  # Check if the result is not None
         result, email = odgovori(opcija)
         if result:
-            with st.chat_message("assistant", avatar=avatar_ai):
-                message_placeholder = st.empty()
-                full_response = ""
-                for response in client.chat.completions.create(
-                    model="gpt-4-turbo-preview",
-                    messages=[
-                        {"role": "system", "content": "[Use only the Serbian language] You are an expert in business data analysis. Analyze the document. Think critically and do business analysis of the company. The accent is on GAP analysis. "},
-                        {"role": "user", "content": f"Write your report based on this input: {result}"}
-                    ],
-                    stream=True,
-                ):
-                    full_response += (response.choices[0].delta.content or "")
-                    message_placeholder.markdown(full_response + "▌")
-                message_placeholder.markdown(full_response)
-                predlozi = recommended(full_response)
-                
-            with st.chat_message("assistant", avatar=avatar_ai):
-                message_placeholder = st.empty()
-                recommendation_response = ""
-                for response in client.chat.completions.create(
-                    model="gpt-4-turbo-preview",
-                    messages=[
+            # prva faza citanje odgovora i komentar
+            gap_message=[
+                {"role": "system", "content": "[Use only the Serbian language] You are an expert in business data analysis. Analyze the document. Think critically and do business analysis of the company. The accent is on GAP analysis. "},
+                {"role": "user", "content": f"Write your report based on this input: {result}"}
+            ]
+            full_response = positive_agent(gap_message)
+            predlozi = recommended(full_response)
+            # druga faza preporuke na osnovu portfolia
+            recommend_message=[
                         {"role": "system", "content": "[Use only the Serbian Language] You are an experienced digital transformation consultant. You are working for company Positive doo, the leader in Digital Transformation services in Serbia."},
                         {"role": "user", "content": f"Based on previous GAP analysis: {full_response}, make suggestions for business improvement of the descibed business process. Suggest solutins based on the text from portfolio of your company Positive doo: {predlozi}"}
-                    ],
-                    stream=True,
-                ):
-                    recommendation_response += (response.choices[0].delta.content or "")
-                    message_placeholder.markdown(recommendation_response + "▌")
-                message_placeholder.markdown(recommendation_response)
-                
-            st.title('Polar Chart Example')
-            labels = ['Poslovna zrelostt', 'Digitalna zrelost', 'Upotreba AI', 'Sajber bezbednost', 'IT infrastruktura']
-            data = [4, 3, 4, 2, 5]
-            num_vars = len(data)
-            # Plotting
-            radar_fig = create_radar_chart(data, labels, num_vars)
-            radar_fig.savefig('radar_chart.png', bbox_inches='tight')
-            # Display in Streamlit
-            st.pyplot(radar_fig)
-            gap_analiza = full_response + "\n\n" + recommendation_response + "\n\n" + "Ovde ce biti i grafikoni..."
-            
-            # sledi formatiranje i slanje maila
-            posalji_mail(email, gap_analiza, 'radar_chart.png')
+            ]
+            recommendation_response = positive_agent(recommend_message)    
+            # treca faza kreiranje dokumenta
+            grafikon = show_graph()
+            gap_analiza = full_response + "\n\n" + recommendation_response + "\n\n"
+            # cetvrta faza slanje maila
+            posalji_mail(email, gap_analiza, grafikon)
                 
 if __name__ == "__main__":
     main()
