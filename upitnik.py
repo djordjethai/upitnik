6364e8ae41c5fc2907633aeebd5ddd99ac9aad14
Djordje <djordje.thai@gmail.com>
2024-04-16 10:22:03 +0700

    sa starom funkcijom, promenje i req i packages

--- upitnik.py
+++ upitnik.py
@@ -1,9 +1,11 @@
 ﻿import matplotlib.pyplot as plt
 import numpy as np
 import os
-import pypandoc
 import streamlit as st
-
+import markdown
+from html2docx import html2docx
+import io
+import pdfkit
 from docx import Document
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Pt
@@ -17,74 +19,111 @@ from smtplib import SMTP
 
 from myfunc.retrievers import HybridQueryProcessor
 
+
 #from myfunc.mojafunkcija import send_email
 
 client=OpenAI()
 avatar_ai="bot.png" 
 
+# modifikovano iz myfunc.mojafunkcija (umesto downlaod ide save pdf)
+def sacuvaj_dokument(content, file_name):
+    """
+    Saves a markdown content as a text, DOCX, and PDF file, providing options to download each format.
 
-# Convert Markdown content to a PDF file
-def create_pdf(content, image_path=None, filename="analysis_report.pdf"):
-    # Include the image in Markdown format if provided
-    if image_path:
-        image_md = f"![Embedded Image]({os.path.abspath(image_path)})"
-        content += "\n\n" + image_md
+    Args:
+    content (str): The markdown content to be saved.
+    file_name (str): The base name for the output files.
 
-    # Convert Markdown to PDF using Pandoc
-    pypandoc.convert_text(content, 'pdf', format='md', outputfile=filename, extra_args=['-V', 'geometry:margin=1in'])
-    return filename
-
-
-# docx koji sadrzi formatirani tekst (i grafikon)
-def create_docx(content, image_path=None, filename="formatted_document.docx"):
-    doc = Document()
-    # Define custom styles for document if not already present
-    styles = doc.styles
-    if 'Heading 1' not in styles:
-        heading1 = styles.add_style('Heading 1', 1)
-        heading1.font.size = Pt(24)
-
-    if 'Heading 2' not in styles:
-        heading2 = styles.add_style('Heading 2', 1)
-        heading2.font.size = Pt(20)
-
-    # Parse Markdown and apply styles
-    lines = content.split('\n')
-    for line in lines:
-        if line.startswith('### '):
-            p = doc.add_paragraph(line[4:], style='Heading 1')
-        elif line.startswith('#### '):
-            p = doc.add_paragraph(line[5:], style='Heading 2')
-        elif line.startswith('- '):
-            p = doc.add_paragraph(line[2:], style='List Bullet')
-        elif '**' in line:
-            # Handle bold within line
-            p = doc.add_paragraph()
-            parts = line.split('**')
-            bold = False
-            for part in parts:
-                run = p.add_run(part)
-                if bold:
-                    run.bold = True
-                bold = not bold
-        else:
-            p = doc.add_paragraph(line)
-
+    This function converts the markdown content into HTML, then into a DOCX document
+    and a PDF file. It justifies the paragraphs in the DOCX document. The function 
+    also provides Streamlit download buttons for each file format: text, DOCX, and PDF.
+    The function assumes an environment where Streamlit, markdown, html2docx, and pdfkit
+    libraries are available, and uses UTF-8 encoding for the text file.
+    """
+    st.info("Čuva dokument")
+    options = {
+        "encoding": "UTF-8",  # Set the encoding to UTF-8
+        "no-outline": None,
+        "quiet": "",
+    }
+    
+    html = markdown.markdown(content)
+    buf = html2docx(html, title="Content")
+    # Creating a document object
+    doc = Document(io.BytesIO(buf.getvalue()))
+    # Iterate over the paragraphs and set them to justified
     for paragraph in doc.paragraphs:
         paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-    # Add image if provided
-    if image_path:
-        doc.add_picture(image_path, width=Pt(300))  # Width is just an example
+    # Creating a byte buffer object
+    doc_io = io.BytesIO()
+    doc.save(doc_io)
+    doc_io.seek(0)  # Rewind the buffer to the beginning
+    pdfkit.from_string(html, file_name, options=options)
+    
+   
 
-    # Save the document
-    doc.save(filename)
-    return filename
+# Convert Markdown content to a PDF file
+# def create_pdf(content, image_path=None, filename="analysis_report.pdf"):
+#     # Include the image in Markdown format if provided
+#     if image_path:
+#         image_md = f"![Embedded Image]({os.path.abspath(image_path)})"
+#         content += "\n\n" + image_md
+
+#    # Convert Markdown to PDF using Pandoc
+    
+#     return filename
+
+
+# # docx koji sadrzi formatirani tekst (i grafikon)
+# def create_docx(content, image_path=None, filename="formatted_document.docx"):
+#     doc = Document()
+#     # Define custom styles for document if not already present
+#     styles = doc.styles
+#     if 'Heading 1' not in styles:
+#         heading1 = styles.add_style('Heading 1', 1)
+#         heading1.font.size = Pt(24)
+
+#     if 'Heading 2' not in styles:
+#         heading2 = styles.add_style('Heading 2', 1)
+#         heading2.font.size = Pt(20)
+
+#     # Parse Markdown and apply styles
+#     lines = content.split('\n')
+#     for line in lines:
+#         if line.startswith('### '):
+#             p = doc.add_paragraph(line[4:], style='Heading 1')
+#         elif line.startswith('#### '):
+#             p = doc.add_paragraph(line[5:], style='Heading 2')
+#         elif line.startswith('- '):
+#             p = doc.add_paragraph(line[2:], style='List Bullet')
+#         elif '**' in line:
+#             # Handle bold within line
+#             p = doc.add_paragraph()
+#             parts = line.split('**')
+#             bold = False
+#             for part in parts:
+#                 run = p.add_run(part)
+#                 if bold:
+#                     run.bold = True
+#                 bold = not bold
+#         else:
+#             p = doc.add_paragraph(line)
+
+#     for paragraph in doc.paragraphs:
+#         paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+#     # Add image if provided
+#     if image_path:
+#         doc.add_picture(image_path, width=Pt(300))  # Width is just an example
+
+#     # Save the document
+#     doc.save(filename)
+#     return filename
 
 
 # Function to send email, adjusted for the new PDF generation
 def posalji_mail(email, gap_analiza, image_path):
     st.info(f"Sending email to {email}")
-    pdf_path = create_pdf(gap_analiza, image_path)
+    pdf_path = "analysis_report.pdf"
     send_email(
         subject="Gap Analysis Report",
         message="Please find attached the gap analysis report, which includes the radar chart.",
@@ -101,23 +140,23 @@ def posalji_mail(email, gap_analiza, image_path):
 
 
 # salje mejl
-def posalji_mail2(email, gap_analiza, image_path):
-    st.info(f"Saljem email na adresu {email}")
-    doc_path = create_docx(gap_analiza, image_path)  # Now also passing the image path
+# def posalji_mail2(email, gap_analiza, image_path):
+#     st.info(f"Saljem email na adresu {email}")
+#     doc_path = create_docx(gap_analiza, image_path)  # Now also passing the image path
 
-    # Assuming your send_email function can handle attachments and is defined as shown previously
-    send_email(
-        subject="Gap Analiza",
-        message="Please find attached the gap analysis document, which includes the radar chart.",
-        from_addr="azure.test@positive.rs",
-        to_addr=email,
-        smtp_server="smtp.office365.com",
-        smtp_port=587,
-        username="azure.test@positive.rs",
-        password=os.getenv("PRAVNIK_PASS"),
-        attachments=[doc_path]  # Only the document needs to be attached now, as it includes the image
-    )
-    st.info(f"Poslat je email na adresu {email}")
+#     # Assuming your send_email function can handle attachments and is defined as shown previously
+#     send_email(
+#         subject="Gap Analiza",
+#         message="Please find attached the gap analysis document, which includes the radar chart.",
+#         from_addr="azure.test@positive.rs",
+#         to_addr=email,
+#         smtp_server="smtp.office365.com",
+#         smtp_port=587,
+#         username="azure.test@positive.rs",
+#         password=os.getenv("PRAVNIK_PASS"),
+#         attachments=[doc_path]  # Only the document needs to be attached now, as it includes the image
+#     )
+#     st.info(f"Poslat je email na adresu {email}")
 
 
 # Adjusted mail sending function to attach PDF
@@ -281,6 +320,7 @@ def main():
             # treca faza kreiranje dokumenta
             grafikon = show_graph()
             gap_analiza = full_response + "\n\n" + recommendation_response + "\n\n"
+            sacuvaj_dokument(gap_analiza, "analysis_report.pdf")
             # cetvrta faza slanje maila
             posalji_mail(email, gap_analiza, grafikon)
             try:    
